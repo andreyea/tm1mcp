41cda3ffaa4948f4c5855a38263381185220c104
andreyea <andy86e@gmail.com>
2025-05-09 19:48:41 +1000

    Added mdx tools and mdxpy

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from server import mcp
 
 # Import tools to register them in the MCP server
 import tools.cube_tools
+import tools.cell_tools
+import tools.mdx_tools
 
 if __name__ == "__main__":
     mcp.run()
---
+++ tools/cell_tools.py
@@ -0,0 +1,3 @@
+from server import mcp
+from TM1py import TM1Service
+from config import TM1_CONFIG
